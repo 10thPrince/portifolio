4b4fdaf632f5340462a20f37e6900acb7c14a88e
10thPrince <ntwariprince5050@gmail.com>
2025-01-23 22:50:29 +0200

    finished experience part

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Skills from './components/Skills'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </>
   )
 }
